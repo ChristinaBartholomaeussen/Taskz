856bb049e2e871d7e29e0bb506c190c3b6886b2d
OscarOtterstad <oscar.vinther@gmail>
2020-11-26 16:45:19 +0100

    Implementere popup funktion for new project, og derudover commentere det.

--- src/main/java/dk/kea/taskz/Controllers/LoginController.java
+++ src/main/java/dk/kea/taskz/Controllers/LoginController.java
@@ -3,6 +3,7 @@ package dk.kea.taskz.Controllers;
 import dk.kea.taskz.Services.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
@@ -12,7 +13,8 @@ public class LoginController
 {
     @Autowired
     private MemberService memberService;
-
+    
+    
     @GetMapping({"/","/login"})
     public String login()
     {
@@ -20,15 +22,17 @@ public class LoginController
     }
 
     @PostMapping("/postLogin")
-    public String postLogin(WebRequest data)
+	public String postLogin(WebRequest data)
     {
+	
         String username = data.getParameter("username");
         String password = data.getParameter("password");
+        
+        if(memberService.verifyLogin(username,password)) {
+            return "redirect:/projects";
+		}
 
-        if(memberService.verifyLogin(username,password))
-            return "projects";
-
-        System.out.println("Wrong password");
+        
         return "redirect:/login";
     }
 
---
+++ src/main/java/dk/kea/taskz/Controllers/ProjectController.java
@@ -0,0 +1,34 @@
+package dk.kea.taskz.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProjectController {
+
+	/**
+	 * Getmapping for project. 
+	 * Sætter en model attribute med en boolean som styre om popup html skal være aktiv
+	 * @param model
+	 * @return projects
+	 */
+	@GetMapping("/projects")
+	public String projets(Model model) {
+		model.addAttribute("popup", false);
+		return "projects";
+	}
+
+	/**
+	 * En ny getmapping som bliver kaldt af "New Projects" linket i project siden.
+	 * Egentlig returnere den projekt siden igen, men også sætter boolean til true.
+	 * Det gør at popup bliver aktiv.
+	 * @param model
+	 * @return projects
+	 */
+	@GetMapping("/newProject")
+	public String newProject(Model model){
+		model.addAttribute("popup", true);
+		return "projects";
+	}
+}
