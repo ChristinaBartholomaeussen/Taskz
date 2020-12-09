9aa40875ae91c16b32efb133155321881818e888
OscarOtterstad <oscar.vinther@gmail>
2020-12-09 15:59:11 +0100

    Laver en cute lille gantt chart udfra subprojects xD <<33

--- src/main/java/dk/kea/taskz/Controllers/DiagramController.java
+++ src/main/java/dk/kea/taskz/Controllers/DiagramController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
@@ -21,6 +23,8 @@ public class DiagramController
     @Autowired
     SubprojectService subprojectService;
 
+    int activeProjectID = -1;
+    
     @GetMapping("/Gant")
     public String gant(Model model)
     {
@@ -36,9 +40,17 @@ public class DiagramController
         return "/Gant";
     }
     
+    @PostMapping("/GantSubProjectPost")
+	public String gantSubProjectPost(WebRequest data) {
+		activeProjectID = Integer.valueOf(data.getParameter("activeProjectIDdada"));
+		return "redirect:/GantSubProject";
+	}
+    
+    
     @GetMapping("/GantSubProject")
 	public String gantSubProject(Model model) {
     	
+    	model.addAttribute("subprojectsList", subprojectService.getAllAssociatedSubprojects(activeProjectID));
     	return "gantSubProject";
 	}
 }
