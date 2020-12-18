5c33a67296c42b47d5d3d7eb23cedd48891e8256
christina bartholomæussen <chri782z@edu.ucl.dk>
2020-12-18 12:38:43 +0100

    kommentar til TimeService

--- src/main/java/dk/kea/taskz/Services/TimeService.java
+++ src/main/java/dk/kea/taskz/Services/TimeService.java
@@ -9,22 +9,43 @@ import java.time.LocalDate;
 @Service
 public class TimeService {
 
+    /**(CMB)
+     * Method to check wheter the startDato and deadline for a new subproject
+     * is between or equal to the chosen projects startDate and deadline.
+     * It takes the a project as parameters and two LocalDates startdate and deadline.
+     * If the startdate for subproject and dedline is not between it will return false.
+     *
+     * @param project
+     * @param startdate
+     * @param deadline
+     * @return
+     */
+
    public boolean isSubprojectStartDateAndDeadlineBetweenProject(Project project, LocalDate startdate, LocalDate deadline){
 
-       if(startdate.isBefore(project.getStartDate()) || deadline.isAfter(project.getDeadline()))
+       if(startdate.isBefore(project.getStartDate()) && deadline.isAfter(project.getDeadline()))
        {
-
            return false;
        }
 
        return true;
     }
 
+
+    /**(CMB)
+     * Method to check if the deadline for a task is between the
+     * chosen subproject. It checks if the deadline is equal to
+     * deadline and startdate for the subproject too, to make
+     * sure that the tasks is created correct
+     *
+     * @param subproject
+     * @param deadline
+     * @return
+     */
     public boolean isTaskDeadlineBetweenSubprojectStartDateAndDeadline(Subproject subproject, LocalDate deadline){
 
        if(deadline.isAfter(subproject.getSubprojectStartDate()) && deadline.isBefore(subproject.getSubprojectDeadline()))
         {
-
            return true;
 
         }else if(deadline.isEqual(subproject.getSubprojectDeadline()) || deadline.isEqual(subproject.getSubprojectStartDate())){
@@ -34,6 +55,16 @@ public class TimeService {
         return false;
     }
 
+    /**(CMB)
+     * Method to check is the deadline is before startdate.
+     * Can be used for both projects and subproject, that's
+     * why it takes two Localdates and not a Project and subproject
+     * as parameters.
+     *
+     * @param startdate
+     * @param deadline
+     * @return
+     */
     public boolean isDeadlineBeforeStartDate(LocalDate startdate, LocalDate deadline){
 
        if(startdate.isBefore(deadline)){
